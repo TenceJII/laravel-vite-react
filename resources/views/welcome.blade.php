51afdb8f812a41d2338a7b8b388eaf179ed8202e
TenceJII <109458478+TenceJII@users.noreply.github.com>
2024-07-09 17:42:39 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite('resources/css/app.css')
+        <title>Laravel</title>
+    </head>
+    <body >
+        <div class="p-4 font-bold text-red-800 bg-slate-300">
+            halo
+        </div>
+        <div class="text-red-500 mt-52 ">
+            halo
+        </div>
+    </body>
+</html>
